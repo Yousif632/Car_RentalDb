f6f0d04de8f72e08ecb3410c6a3602cea94f4371
Yousif Sediqi <ac122766@avcol.school.nz>
2024-03-31 06:31:00 +1300

    Add project files.

---
+++ Car_RentalDb/Areas/Identity/Data/Car_RentalDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Car_RentalDb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_RentalDb.Areas.Identity.Data;
+
+public class Car_RentalDbContext : IdentityDbContext<ApplicationUser>
+{
+    public Car_RentalDbContext(DbContextOptions<Car_RentalDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
