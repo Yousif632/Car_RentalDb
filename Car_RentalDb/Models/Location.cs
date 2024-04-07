5c8766e5e6b3bbbf0a724118688e4f7add079e2f
Yousif Sediqi <ac122766@avcol.school.nz>
2024-04-08 01:30:48 +1200

    Added a validation to location

--- Car_RentalDb/Models/Location.cs
+++ Car_RentalDb/Models/Location.cs
@@ -5,11 +5,16 @@ namespace Car_RentalDb.Models
     public class Location
     {
         public int LocationID { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Address must be between 5 and 100 characters")]
         public string Address { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         [Required]
         [StringLength(10)]
         public string City { get; set; }
+        [Required]
+        [Range(1000, 2000, ErrorMessage = "Please enter a vaild model year (From 1000 - 2000")]
 
         public int Zip { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
